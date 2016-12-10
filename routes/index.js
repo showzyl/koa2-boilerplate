fa9ebe4b22a8ca6996df9c3c4c2d041f369548cf
showzyl <showzyl520@gmail.com>
2016-12-10 21:50:45 +0800

    add log4
    add log4

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,21 @@
 
 'use strict'
 
-
+const log4js = require('log4js');
 const router = require('koa-router')();
-
 const article = require('./article');
 
+
+log4js.loadAppender('file');
+var logger = log4js.getLogger('index');
+logger.info('index is in.');
+
 router.get('/', async function (ctx, next) {
+	logger.info('success');
 	ctx.state = {
 		title: 'koa2 title'
 	};
-
+	
 	await ctx.render('index', {
 		user: 'lizi'
 	});
